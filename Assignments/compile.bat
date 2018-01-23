caa9afbb22b8c15657071a6cf2d02aac0a002aaa
Paul Vesey <pvesey@users.noreply.github.com>
2018-01-23 19:10:32 +0000

    First Commit

---
+++ Assignments/compile.bat
@@ -0,0 +1,31 @@
+cls
+xelatex Practical1.tex
+copy Practical1.pdf output\"Practical 1.pdf"
+
+xelatex Practical2.tex
+copy Practical2.pdf output\"Practical 2.pdf"
+
+xelatex Practical3.tex
+copy Practical3.pdf output\"Practical 3.pdf"
+
+xelatex Practical4.tex
+copy Practical4.pdf output\"Practical 4.pdf"
+
+xelatex Practical5.tex
+copy Practical5.pdf output\"Practical 5.pdf"
+
+xelatex Practical6.tex
+copy Practical6.pdf output\"Practical 6.pdf"
+
+xelatex Practical7.tex
+copy Practical7.pdf output\"Practical 7.pdf"
+
+xelatex Practical8.tex
+copy Practical8.pdf output\"Practical 8.pdf"
+
+xelatex Practical9.tex
+copy Practical9.pdf output\"Practical 9.pdf"
+
+xelatex Practical10.tex
+copy Practical10.pdf output\"Practical 10.pdf"
+
