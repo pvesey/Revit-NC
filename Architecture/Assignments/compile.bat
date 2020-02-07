5b26807f0a64cc57097771544e5c6158045af9c3
Paul Vesey <pvesey@users.noreply.github.com>
2020-02-07 16:22:15 +0000

    Reconfig Arch Assignments

--- Architecture/Assignments/compile.bat
+++ Architecture/Assignments/compile.bat
@@ -1,9 +1,16 @@
 cls
 xelatex Assignment1.tex
-copy Assignment1.pdf output\"Practical 1.pdf"
+copy Assignment1.pdf output\RevitArch-Asmt1.pdf
+copy Assignment1.pdf ..\Assignment-1-Pack\RevitArch-Asmt1.pdf
 
 xelatex Assignment2.tex
-copy Assignment2.pdf output\"Practical 2.pdf"
+copy Assignment2.pdf output\RevitArch-Asmt2.pdf
+copy Assignment2.pdf ..\Assignment-2-Pack\RevitArch-Asmt2.pdf
 
 xelatex Assignment3.tex
-copy Assignment3.pdf output\"Practical 3.pdf"
+copy Assignment3.pdf output\RevitArch-Asmt3.pdf
+copy Assignment3.pdf ..\Assignment-3-Pack\RevitArch-Asmt3.pdf
+
+xelatex Assignment4.tex
+copy Assignment4.pdf output\RevitArch-Asmt4.pdf
+copy Assignment4.pdf ..\Assignment-4-Pack\RevitArch-Asmt4.pdf
---
+++ Architecture/CompressFolders.ps1
@@ -0,0 +1,3 @@
+Compress-Archive .\Assignment-1-Pack\ ArchAssignment1-Pack.zip
+Compress-Archive .\Assignment-2-Pack\ ArchAssignment2-Pack.zip
+Compress-Archive .\Assignment-3-Pack\ ArchAssignment3-Pack.zip
