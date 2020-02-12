f9e7a667e78d68d65098f9bc620cde1f784cbd64
Paul Vesey <pvesey@users.noreply.github.com>
2020-02-12 12:25:23 +0000

    Updates to Fonts etc

--- Architecture/Assignments/compile.bat
+++ Architecture/Assignments/compile.bat
@@ -10,7 +10,3 @@ copy Assignment2.pdf ..\Assignment-2-Pack\RevitArch-Asmt2.pdf
 xelatex Assignment3.tex
 copy Assignment3.pdf output\RevitArch-Asmt3.pdf
 copy Assignment3.pdf ..\Assignment-3-Pack\RevitArch-Asmt3.pdf
-
-xelatex Assignment4.tex
-copy Assignment4.pdf output\RevitArch-Asmt4.pdf
-copy Assignment4.pdf ..\Assignment-4-Pack\RevitArch-Asmt4.pdf
