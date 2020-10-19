f26eec876c599051f789b86cc19c21279702330a
Paul Vesey <pvesey@users.noreply.github.com>
2020-10-19 13:58:06 +0100

    Update to Arch Assignments
    Updated assignments to Autumn 2020 and deployed PAS naming convention

--- Architecture/CompressFolders.ps1
+++ Architecture/CompressFolders.ps1
@@ -1,3 +1,3 @@
-Compress-Archive .\Assignment-1-Pack\ ArchAssignment1-Pack.zip
-Compress-Archive .\Assignment-2-Pack\ ArchAssignment2-Pack.zip
-Compress-Archive .\Assignment-3-Pack\ ArchAssignment3-Pack.zip
+Compress-Archive .\Assignment-1-Pack\ RARC01-LIT-00-ZZ-IE-A-001-A1-P01.zip
+Compress-Archive .\Assignment-2-Pack\ RARC02-LIT-00-ZZ-IE-A-001-A1-P01.zip
+Compress-Archive .\Assignment-3-Pack\ RARC03-LIT-00-ZZ-IE-A-001-A1-P01.zip
