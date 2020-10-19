0b38bf10f57e0e7cd27221acaca4203ea52e1052
Paul Vesey <pvesey@users.noreply.github.com>
2020-10-19 18:09:25 +0100

    Revit MEP Update in Progress

--- MEP/Assignments/compile.bat
+++ MEP/Assignments/compile.bat
@@ -1,16 +1,16 @@
 cls
 xelatex Assignment1.tex
-copy Assignment1.pdf output\RevitMEP-Asmt1.pdf
-copy Assignment1.pdf ..\Assignment-1-Pack\RevitMEP-Asmt1.pdf
+copy Assignment1.pdf output\RMEP01-LIT-00-ZZ-SP-A-001-A1-P01.pdf
+copy Assignment1.pdf ..\Assignment-1-Pack\RMEP01-LIT-00-ZZ-SP-A-001-A1-P01.pdf
 
 xelatex Assignment2.tex
-copy Assignment2.pdf output\RevitMEP-Asmt2.pdf
-copy Assignment2.pdf ..\Assignment-2-Pack\RevitMEP-Asmt2.pdf
+copy Assignment2.pdf output\RMEP02-LIT-00-ZZ-SP-A-001-A1-P01.pdf
+copy Assignment2.pdf ..\Assignment-2-Pack\RMEP01-LIT-00-ZZ-SP-A-001-A1-P01.pdf
 
 xelatex Assignment3.tex
-copy Assignment3.pdf output\RevitMEP-Asmt3.pdf
-copy Assignment3.pdf ..\Assignment-3-Pack\RevitMEP-Asmt3.pdf
+copy Assignment3.pdf output\RMEP03-LIT-00-ZZ-SP-A-001-A1-P01.pdf
+copy Assignment3.pdf ..\Assignment-3-Pack\RMEP01-LIT-00-ZZ-SP-A-001-A1-P01.pdf
 
 xelatex Assignment4.tex
-copy Assignment4.pdf output\RevitMEP-Asmt4.pdf
-copy Assignment4.pdf ..\Assignment-4-Pack\RevitMEP-Asmt4.pdf
+copy Assignment4.pdf output\RMEP04-LIT-00-ZZ-SP-A-001-A1-P01.pdf
+copy Assignment4.pdf ..\Assignment-4-Pack\RMEP01-LIT-00-ZZ-SP-A-001-A1-P01.pdf
--- MEP/CompressFolders.ps1
+++ MEP/CompressFolders.ps1
@@ -1,4 +1,4 @@
-Compress-Archive .\Assignment-1-Pack\ Assignment1-Pack.zip
-Compress-Archive .\Assignment-2-Pack\ Assignment2-Pack.zip
-Compress-Archive .\Assignment-3-Pack\ Assignment3-Pack.zip
-Compress-Archive .\Assignment-4-Pack\ Assignment4-Pack.zip
+Compress-Archive .\Assignment-1-Pack\ RMEP01-LIT-00-ZZ-IE-A-001-A1-P01.zip
+Compress-Archive .\Assignment-2-Pack\ RMEP02-LIT-00-ZZ-IE-A-001-A1-P01.zip
+Compress-Archive .\Assignment-3-Pack\ RMEP03-LIT-00-ZZ-IE-A-001-A1-P01.zip
+Compress-Archive .\Assignment-4-Pack\ RMEP04-LIT-00-ZZ-IE-A-001-A1-P01.zip
