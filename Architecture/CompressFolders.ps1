e0490839a3f73392d826648b69e18dc342aa2dcd
Paul Vesey <pvesey@users.noreply.github.com>
2021-02-10 16:50:09 +0000

    Assignment Updates

--- Architecture/CompressFolders.ps1
+++ Architecture/CompressFolders.ps1
@@ -1,3 +1,3 @@
-Compress-Archive .\Assignment-1-Pack\ RARC01-LIT-00-ZZ-IE-A-001-A1-P01.zip
-Compress-Archive .\Assignment-2-Pack\ RARC02-LIT-00-ZZ-IE-A-001-A1-P01.zip
-Compress-Archive .\Assignment-3-Pack\ RARC03-LIT-00-ZZ-IE-A-001-A1-P01.zip
+Compress-Archive .\Assignment-1-Pack\ RARC01-LIT-00-ZZ-IE-A-001-A1-P01.zip -update
+Compress-Archive .\Assignment-2-Pack\ RARC02-LIT-00-ZZ-IE-A-001-A1-P01.zip -update
+Compress-Archive .\Assignment-3-Pack\ RARC03-LIT-00-ZZ-IE-A-001-A1-P01.zip -update
