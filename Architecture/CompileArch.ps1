a2773553c6f277dc8b68040684f4d5d0bdb01e0d
Paul Vesey <pvesey@users.noreply.github.com>
2021-05-21 14:28:36 +0100

    Arch Cleanup

--- Architecture/CompileArch.ps1
+++ Architecture/CompileArch.ps1
@@ -1,22 +1,21 @@
 # Cleanout folders
-#Remove-Item .\Assignment-1-Pack\*.*
-#Remove-Item .\Assignment-2-Pack\*.*
-#Remove-Item .\Assignment-3-Pack\*.*
-#Remove-Item .\Assignment-4-Pack\*.*
+#Remove-Item .\RARC01\*.*
+#Remove-Item .\RARC02\*.*
+#Remove-Item .\RARC03\*.*
 #
 #
-## Copy Items to Pack Folders
-#
-#Copy-Item .\Assets\LIT-A1-Metric.rfa .\Assignment-1-Pack\LIT-A1-Metric.rfa -Force
-#Copy-Item .\Assets\LIT-A1-Metric.rfa .\Assignment-2-Pack\LIT-A1-Metric.rfa -Force
-#Copy-Item .\Assets\LIT-A1-Metric.rfa .\Assignment-3-Pack\LIT-A1-Metric.rfa -Force
-#Copy-Item .\Assets\LIT-A1-Metric.rfa .\Assignment-4-Pack\LIT-A1-Metric.rfa -Force
+# Copy Items to Pack Folders
+
+Copy-Item .\Assets\LIT-A1-Metric-Arch.rfa .\RARC01\LIT-A1-Metric-Arch.rfa -Force
+Copy-Item .\Assets\LIT-A1-Metric-Arch.rfa .\RARC02\LIT-A1-Metric-Arch.rfa -Force
+Copy-Item .\Assets\LIT-A1-Metric-Arch.rfa .\RARC03\LIT-A1-Metric-Arch.rfa -Force
+
 #Write-Host 'A1 Sheet Copied to Packs'
 #
-#Copy-Item .\Assets\RMEP01-LIT-00-ZZ-M3-A-001-A1-P02.rvt .\Assignment-1-Pack\RMEP01-LIT-00-ZZ-M3-A-001-A1-P02.rvt -Force
-#Copy-Item .\Assets\RMEP01-LIT-00-ZZ-M3-A-001-A1-P02.rvt .\Assignment-2-Pack\RMEP02-LIT-00-ZZ-M3-A-001-A1-P02.rvt -Force
-#Copy-Item .\Assets\RMEP01-LIT-00-ZZ-M3-A-001-A1-P02.rvt .\Assignment-3-Pack\RMEP03-LIT-00-ZZ-M3-A-001-A1-P02.rvt -Force
-#Copy-Item .\Assets\RMEP01-LIT-00-ZZ-M3-A-001-A1-P02.rvt .\Assignment-4-Pack\RMEP04-LIT-00-ZZ-M3-A-001-A1-P02.rvt -Force
+#Copy-Item .\Assets\RMEP01-LIT-00-ZZ-M3-A-001-A1-P02.rvt .\RARC01\RMEP01-LIT-00-ZZ-M3-A-001-A1-P02.rvt -Force
+#Copy-Item .\Assets\RMEP01-LIT-00-ZZ-M3-A-001-A1-P02.rvt .\RARC02\RMEP02-LIT-00-ZZ-M3-A-001-A1-P02.rvt -Force
+#Copy-Item .\Assets\RMEP01-LIT-00-ZZ-M3-A-001-A1-P02.rvt .\RARC03\RMEP03-LIT-00-ZZ-M3-A-001-A1-P02.rvt -Force
+
 #Write-Host 'Arch Model Copied to Packs'
 #
 #Copy-Item .\Assets\RMEP01\RMEP01-E-101-A1-.pdf .\Assignment-1-Pack\RMEP01-LIT-00-ZZ-DR-E-101-A1-P02.pdf -Force
@@ -82,5 +81,5 @@ Compress-Archive .\RARC02\ $RARC02Archive -Update
 Compress-Archive .\RARC03\ $RARC03Archive -Update
 
 
-Write-Host -NoNewLine 'Press any key to continue...';
+Write-Host -NoNewLine 'Script Complete: Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
